93cb5fcb5b24cf8c3c8bb3f7dbc6655998ed3fd3
dacrands <davecrands@gmail.com>
2021-07-22 15:53:21 -0500

    Add Home

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'
+import initListings from '../data/initListings'
+import Listings from './Listings';
+
+export default function Home() {
+    useEffect(() => {
+        initListings();
+    }, [])
+    return (
+        <div>
+            <header>
+                <h1>Phonebook App</h1>
+            </header>
+            <Listings />
+        </div>
+    );
+}
