2907791315ed133b0a72ff18fa17c062821c3e41
dacrands <davecrands@gmail.com>
2021-07-22 13:55:31 -0500

    Remove uniqueId call

--- src/data/initListings.js
+++ src/data/initListings.js
@@ -1,13 +1,15 @@
 import { uniqueId } from 'lodash'
 
 export default function initListings() {
-    if (!localStorage.getItem("listings")) {
-      localStorage.setItem("listings", JSON.stringify([{
-        id: uniqueId(),
+  if (!localStorage.listings) {
+    localStorage.setItem("listings", JSON.stringify([
+      {
+        id: 1,
         firstName: "first",
         lastName: "last",
         phone: "999-999-9999",
         email: "test@test.com"
-      }]))
-    }    
+      }
+    ]))    
+  }    
 }
